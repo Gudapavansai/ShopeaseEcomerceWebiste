6593bf58225585c22ce0d39ddd595f31f3af41ea
Guda pavansai <159716840+Gudapavansai@users.noreply.github.com>
2026-01-30 16:12:56 +0530

    Fix footer logo responsiveness and remove gap for mobile devices

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,9 +10,9 @@ const Footer = () => {
         
         {/* Left Section */}
         <div>
-            <Link to="/" className="flex items-center">
-                   <img src={assets.logo} className="w-30" alt="ShopEase" />
-                   <p className="prata-regular text-5xl text-gray-800">ShopEase</p>
+            <Link to="/" className="flex items-center gap-0">
+                   <img src={assets.logo} className="w-12 sm:w-24 md:w-30" alt="ShopEase" />
+                   <p className="prata-regular text-xl sm:text-3xl md:text-5xl text-gray-800">ShopEase</p>
                 </Link>
             <p className='w-full md:w-2/3 text-gray-600'>
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
